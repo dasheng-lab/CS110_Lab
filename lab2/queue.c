65ad67549a694f437d55420c2fe95dfdea0f5076
dasheng-lab <sunshq2024@shanghaitech.edu.cn>
2026-03-12 21:51:38 +0800

    restore repository5

---
+++ lab2/queue.c
@@ -0,0 +1,79 @@
+#include "queue.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+Queue *queue_create(void)
+{
+  Queue *queue = malloc(sizeof(Queue));
+  if (queue == NULL){
+    printf("Error: malloc failed\n");
+    return NULL;
+  }
+  queue->size = 0;
+  queue->capacity = QUEUE_INITIAL_CAPACITY;
+  queue->data = malloc(sizeof(double) * queue->capacity);
+  return queue;
+}
+
+void push(Queue *queue, double element)
+{
+  if(queue == NULL){
+    printf("Error: queue is NULL\n");
+    return;
+  }
+  if (queue->size == queue->capacity)
+  {
+    int capacity = queue->capacity * 2;
+
+    queue->data = realloc(queue->data, sizeof(double) * capacity);
+    if (queue->data == NULL)
+    {
+      queue_free(queue);
+      printf("Error: realloc failed\n");
+      return;
+    }
+    queue->capacity = capacity;
+  }
+
+  int insert_index = queue->size % queue->capacity;
+  queue->data[insert_index] = element;
+  queue->size++;
+}
+
+double back(Queue *queue)
+{
+  if(queue == NULL){
+    printf("Error: queue is NULL\n");
+    return 0.0;
+  }
+  if (queue->size == 0)
+  {
+    printf("Error: queue is empty\n");
+    return 0.0;
+  }
+  return queue->data[queue->size - 1];
+}
+
+void queue_free(Queue *queue)
+{
+  if(queue == NULL){
+    printf("Error: queue is NULL\n");
+    return;
+  }
+  free(queue->data);
+  free(queue);
+}
+
+void another_queue_free(Queue **queue){
+  if(queue == NULL){
+    printf("Error: queue is NULL\n");
+    return;
+  }
+  if (*queue == NULL)
+  {
+    printf("Error: queue is NULL\n");
+    return;
+  }
+  queue_free(*queue);
+  *queue = NULL;
+}
